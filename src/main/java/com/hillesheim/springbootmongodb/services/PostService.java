e4896b06c97e5c760707a5051ea441addac51b3e
Lucas Hillesheim <lucashillesheim11@gmail.com>
2021-09-06 20:46:54 -0300

    193.Getting post by id

---
+++ src/main/java/com/hillesheim/springbootmongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.hillesheim.springbootmongodb.services;
+
+import java.util.Optional;
+
+import com.hillesheim.springbootmongodb.domain.Post;
+import com.hillesheim.springbootmongodb.repository.PostRepository;
+import com.hillesheim.springbootmongodb.services.exception.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
