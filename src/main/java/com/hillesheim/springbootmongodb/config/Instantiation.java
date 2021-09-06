86b8cf33893842ed5da418108931b8bbad1f70a6
Lucas Hillesheim <lucashillesheim11@gmail.com>
2021-09-06 15:03:04 -0300

    182.Instantiation operation database

---
+++ src/main/java/com/hillesheim/springbootmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.hillesheim.springbootmongodb.config;
+
+import java.util.Arrays;
+
+import com.hillesheim.springbootmongodb.domain.User;
+import com.hillesheim.springbootmongodb.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userReposiroty;
+
+    @Override
+    public void run(String... arg0) throws Exception {
+
+        userReposiroty.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+    }
+
+}
