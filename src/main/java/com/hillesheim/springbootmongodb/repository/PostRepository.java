d108fc9c4a470991c3e626d373bf6ef22d5f89a3
Lucas Hillesheim <lucashillesheim11@gmail.com>
2021-09-06 21:24:46 -0300

    196.Query with @Query

--- src/main/java/com/hillesheim/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/hillesheim/springbootmongodb/repository/PostRepository.java
@@ -5,10 +5,15 @@ import java.util.List;
 import com.hillesheim.springbootmongodb.domain.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+
+    List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
